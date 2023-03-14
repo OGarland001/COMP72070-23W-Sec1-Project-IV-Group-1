6dd8ff60f9a5c3bed77fdd0c5547c64d12fab563
Owen Garland <Ogarland9238@conestogac.on.ca>
2023-03-14 08:45:31 -0400

    added 1-6 tests frames

--- Client_Test_Suite/ClientUnitTests.cs
+++ Client_Test_Suite/ClientUnitTests.cs
@@ -1,11 +1,63 @@
+
 namespace Client_Test_Suite
 {
     [TestClass]
     public class ClientUnitTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CLT_UNIT_TEST_001_Testing_Data_Is_Encrypted_ExpectedOutput_Data_Encrypted()
         {
+            //Goal
+            
+            //Arrange
+            //Act
+            //Assert
+
+
+        }
+
+        [TestMethod]
+        public void CLT_UNIT_TEST_002_Client_Recieved_Through_Para_Constructor_ExpectedOutput_FullyCreatedPacket()
+        {
+
+            //Arrange
+            //Act
+            //Assert
+
+
+        }
+        [TestMethod]
+        public void CLT_UNIT_TEST_003_Client_Can_Update_The_HeaderLength_When_Added_ExpectedOutput_Length()
+        {
+            //Arrange
+            //Act
+            //Assert
+
+        }
+
+        [TestMethod]
+        public void CLT_UNIT_TEST_004_Client_Can_Download_A_Image_ExpectedOutput_ImageFile()
+        {
+            //Arrange
+            //Act
+            //Assert
+
+        }
+        [TestMethod]
+        public void CLT_UNIT_TEST_005_Client_Can_Login_To_The_Server_ExpectedOutput_Sucessful_Login()
+        {
+            //Arrange
+            //Act
+            //Assert
+
+        }
+        [TestMethod]
+        public void CLT_UNIT_TEST_006_Client_Can_View_The_Processed_Image_ExpectedOutput_Sucessful_Processed_Image()
+        {
+            //Arrange
+            //Act
+            //Assert
+
         }
     }
 }
