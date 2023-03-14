ee6f914309542950849582a30a390ccd86a744e6
Brodin Collins-Robb <tapeit03@outlook.com>
2023-03-14 08:33:35 -0400

    Setup unit test shells

--- Server_Test_Suite/ServerUnitTests.cs
+++ Server_Test_Suite/ServerUnitTests.cs
@@ -4,7 +4,7 @@ namespace Server_Test_Suite
     public class ServerUnitTests
     {
         [TestMethod]
-        public void TestName_Inputs_ExpectedOutputs()
+        public void TestName01_Inputs_ExpectedOutputs()
         {
             //Arrange
 
@@ -12,5 +12,185 @@ namespace Server_Test_Suite
 
             //Assert
         }
+
+        [TestMethod]
+        public void TestName02_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName03_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName04_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName05_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName06_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName07_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName08_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName09_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName010_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName011_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName012_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [TestMethod]
+        public void TestName013_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName014_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName015_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName016_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName017_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName018_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestName019_Inputs_ExpectedOutputs()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
     }
 }
