a215f275eb6b75744a59b80e8035eb3eb8b12689
Brodin Collins-Robb <tapeit03@outlook.com>
2023-03-15 17:41:14 -0400

    Cleaned Up Files

--- Server/InterfaceFiles/MainWindow.xaml.cs
+++ Server/InterfaceFiles/MainWindow.xaml.cs
@@ -24,5 +24,25 @@ namespace Server
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
