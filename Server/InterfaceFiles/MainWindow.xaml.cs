c721b7c16c905df1627c5ee86556d10e7f707b79
Brodin Collins-Robb <tapeit03@outlook.com>
2023-03-16 00:07:00 -0400

    added some button implementation

--- Server/InterfaceFiles/MainWindow.xaml.cs
+++ Server/InterfaceFiles/MainWindow.xaml.cs
@@ -27,27 +27,35 @@ namespace Server
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            var accountsWindow = new ClientAccountsWindow();
 
+            accountsWindow.Show();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            var accountsLogWindow = new AccountLogsWindow();
 
+            accountsLogWindow.Show();
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
+            var clientsWindow = new ClientListWindow();
 
+            clientsWindow.Show();
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
+            var predictionWindow = new ImagePredictionHistoryWindow();
 
+            predictionWindow.Show();
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            Console.WriteLine("Do Nothin");
         }
     }
 }
