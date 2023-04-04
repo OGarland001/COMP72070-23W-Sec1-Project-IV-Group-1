c6232c9d1bf156bc24285902906888bf9b18e1f3
Owen Garland <Ogarland9238@conestogac.on.ca>
2023-04-04 08:25:47 -0400

    added send and recieve image

--- Server/Server.cs
+++ Server/Server.cs
@@ -255,6 +255,85 @@ namespace Server
             
         }
 
+        private bool sendImage(TcpClient client, NetworkStream stream)
+        {
+            try
+            {
+                string count = (userData.getSendCount()).ToString();
+                string fileName = userData.getUserName() + count + ".jpg";
+                string path = @"../../../Users/" + userData.getUserName() + "/assets/images/" + fileName;
+
+                byte[] imageBuffer = File.ReadAllBytes(path);
+
+                int index = 0;
+                bool lastPacketSent = false;
+                while (!lastPacketSent)
+                {
+                    Packet sendPacket = new Packet();
+                    sendPacket.setHead('2', '1', states.Sending);
+                    byte[] dataBuf = new byte[500];
+
+                    int bytesToCopy = Math.Min(dataBuf.Length, imageBuffer.Length - index);
+                    Array.Copy(imageBuffer, index, dataBuf, 0, bytesToCopy);
+                    index += bytesToCopy;
+
+
+                    // Check if this is the last packet
+                    if (bytesToCopy == 0)
+                    {
+                        Packet lastPacket = new Packet();
+                        lastPacketSent = true;
+                        lastPacket.setHead('2', '1', states.Saving);
+                        byte[] noData = new byte[0];
+                        lastPacket.setData(noData.Length, noData);
+                        lastPacket.SerializeData();
+                        stream.Write(lastPacket.getTailBuffer(), 0, lastPacket.getTailBuffer().Length);
+                        stream.Flush();
+                        continue;
+                    }
+
+                    sendPacket.setData(bytesToCopy, dataBuf);
+                    sendPacket.SerializeData();
+                    stream.Write(sendPacket.getTailBuffer(), 0, sendPacket.getTailBuffer().Length);
+                    stream.Flush();
+                    byte[] recvBuf = new byte[1024];
+                    int amount = stream.Read(recvBuf, 0, recvBuf.Length);
+
+                    byte[] array = new byte[amount];
+                    Array.Copy(recvBuf, array, amount);
+                    Packet recvPacket = new Packet(array);
+
+                    if (lastPacketSent)
+                    {
+                        if (recvPacket.GetHead().getState() != states.Recv)
+                        {
+                            Exception e = new Exception();
+                            throw e;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        if (recvPacket.GetHead().getState() != states.Saving)
+                        {
+                            Exception e = new Exception();
+                            throw e;
+                        }
+                    }
+                }
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+
+
+        }
 
         public void setDetectedObjects(string[,] objects)
         {
