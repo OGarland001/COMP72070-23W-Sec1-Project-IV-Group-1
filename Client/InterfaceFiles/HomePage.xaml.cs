6021e4e87ed04647cc821c07989bbad5de526d45
Brodin Collins-Robb <tapeit03@outlook.com>
2023-04-04 15:00:36 -0400

    Added refresh button to client

--- Client/InterfaceFiles/HomePage.xaml.cs
+++ Client/InterfaceFiles/HomePage.xaml.cs
@@ -48,6 +48,10 @@ namespace Client.InterfaceFiles
 
         private void Upload_an_Image_Click(object sender, RoutedEventArgs e)
         {
+            if (!this.client.checkConnection())
+            {
+                Main.Content = new MainPage(ref this.client);
+            }
             Microsoft.Win32.OpenFileDialog image = new OpenFileDialog();
             image.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*";
             image.FilterIndex = 1;
@@ -65,8 +69,12 @@ namespace Client.InterfaceFiles
 
         private void Analyze_Click(object sender, RoutedEventArgs e)
         {
+            if (!this.client.checkConnection())
+            {
+                Main.Content = new MainPage(ref this.client);
+            }
             //take the image that is uploaded to the screen imagePicture, and then convert to a byte array and setup the packet structure and send 100 bytes at a time to the server
-            
+
             //imagePicture path
             string path = imagePicture.Source.ToString();
             path = path.Substring(8);
@@ -97,5 +105,12 @@ namespace Client.InterfaceFiles
             Analyze.IsEnabled = false;
         }
 
+        private void refresh_Click(object sender, RoutedEventArgs e)
+        {
+            if (!this.client.checkConnection())
+            {
+                Main.Content = new MainPage(ref this.client);
+            }
+        }
     }
 }
